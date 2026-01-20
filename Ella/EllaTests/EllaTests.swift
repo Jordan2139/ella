c0bbb336289c1ce9acc7701bdcac311992b5a7e3
Jordan.#2139 <65438497+Jordan2139@users.noreply.github.com>
2026-01-20 01:06:20 -0500

    chore: scaffold app folders and remove template files

---
+++ Ella/EllaTests/EllaTests.swift
@@ -0,0 +1,17 @@
+//
+//  EllaTests.swift
+//  EllaTests
+//
+//  Created by Jordan Silva on 1/20/26.
+//
+
+import Testing
+@testable import Ella
+
+struct EllaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
